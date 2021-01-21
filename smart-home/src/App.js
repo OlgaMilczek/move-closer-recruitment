3bf58876daf4e5922f7046e2ed5c368eb48a61ba
Olga Nowakowska <o.w.nowakowska@gmail.com>
2021-01-21 19:52:50 +0100

    Adding localStorage to App

--- smart-home/src/App.js
+++ smart-home/src/App.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react'; 
+import { useReducer, useEffect } from 'react'; 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import './App.css';
@@ -10,13 +10,19 @@ import {publicPath} from './config';
 import reducer from './state-manager/reducer';
 import { Home } from './state-manager/class-model';
 import * as actions from './actions';
+import {  checkLocalStorage, setLocalStorage, getLocalStorage } from './state-manager/localStorageManager';
 
 //Import React components. 
 import HomePage from './components/HomePage';
 import RoomPage from './components/RoomPage';
+import LoaderSpinner from './components/Loader';
+
+
+const NAME = 'my-smart-home-app';
+const USER_NAME = 'Olga';
 
 function App() {
-    const [homeState, dispatch] = useReducer(reducer, new Home('Olga'));
+    const [homeState, dispatch] = useReducer(reducer, null);
 
     const addNewRoom = (roomName) => {
         dispatch(actions.addRoomAction(roomName));
@@ -29,6 +35,29 @@ function App() {
     const toggleRoom = (roomId) => {
         dispatch(actions.toggleRoomAction(roomId));
     };
+
+    useEffect(() => {
+        if (homeState === null ) { 
+            if ( checkLocalStorage(NAME)) {
+                const newStorage = getLocalStorage(NAME);
+                dispatch(actions.setNewStorage(newStorage));
+            } else {
+                const newStorage = new Home(USER_NAME);
+                dispatch(actions.setNewStorage(newStorage));
+            }
+        } else {
+            setLocalStorage(NAME, homeState);
+        }
+
+    }, [homeState]);
+
+    if (homeState === null) {
+        return (
+            <div className = 'loader'>
+                <LoaderSpinner />
+            </div>
+        );
+    }
 
     return (
         <div className="App">
